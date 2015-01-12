cab27edeb2ad1afacf7bfe99a0b50877173f4c7a
Mateusz Dobek <mat.dobek@gmail.com>
2015-01-12 21:51:42 +0100

    add routes for photos controller [#85113568]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   devise_ios_rails_for :users
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :photos, only: [:index, :show, :create]
+    end
+  end
 end
