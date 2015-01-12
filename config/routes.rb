4b923173f385b17bbfdfcf134709eee97124922a
Mateusz Dobek <mat.dobek@gmail.com>
2015-01-12 22:11:47 +0100

    changed routes (removed API/V1) [#85113568]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_ios_rails_for :users
 
-  namespace :api, defaults: { format: 'json' } do
-    namespace :v1 do
-      resources :photos, only: [:index, :show, :create]
-    end
-  end
+  root 'photos#index'
+  resources :photos, only: [:index, :show, :create]
 end
