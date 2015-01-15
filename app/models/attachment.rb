c4a04023d27645235059041a399d178de78081de
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-15 13:04:38 +0100

    add '.old' scope to Attachment [#86021994]

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,9 @@
 class Attachment < ActiveRecord::Base
+  OLD_AFTER = 10.minutes
+
   mount_uploader :file, FileUploader
+
+  scope :old, -> { where('created_at < ?', Time.now - OLD_AFTER) }
 
   validates :file, presence: true
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -10,4 +10,23 @@ describe Attachment do
 
     it { is_expected.to validate_presence_of :file }
   end
+
+  describe 'scopes' do
+    describe '.old' do
+      subject { Attachment.old }
+
+      let!(:old_attachment) { create(:attachment, created_at: Time.parse('2015-01-01 10:00 UTC')) }
+      let!(:young_attachment) { create(:attachment, created_at: Time.parse('2015-01-01 10:10 UTC')) }
+
+      before(:each) do
+        freeze_time('2015-01-01 10:15 UTC')
+        stub_const('Attachment::OLD_AFTER', 10.minutes)
+      end
+
+      it 'returns only records older than threshold' do
+        expect(subject).to include(old_attachment)
+        expect(subject).to_not include(young_attachment)
+      end
+    end
+  end
 end
