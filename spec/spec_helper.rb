8981d2deba43a2bda0b7db5cf49ef685a58b87ff
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-15 12:40:08 +0100

    add means to freeze the time in rspec [#86021994]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,11 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 end
+
+def freeze_time(now = Time.now)
+  datetime = DateTime.parse(now.to_s)
+  time = Time.parse(now.to_s)
+
+  allow(DateTime).to receive(:now) { datetime }
+  allow(Time).to receive(:now) { time }
+end
