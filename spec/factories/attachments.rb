4bcbc2a0349f107ac08e275db98fcd982ac14633
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-16 15:02:09 +0100

    refactor tests to account for fixture file change [#86301740]

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :attachment do
-    file Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'attachments', 'files', 'netguru.jpg'))
+    file Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/netguru.jpg")
   end
 
   factory :unsupported_attachment, class: Attachment do
-    file Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'attachments', 'files', 'unsupported_file.bad'))
+    file Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/unsupported_file.bad")
   end
 end
