ff1f3d995a0ca9e32421f689efa4489954a07a93
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-16 11:43:20 +0100

    separate Attachment file path for each environment [#86283696]

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -9,7 +9,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{model.id}"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
---
+++ spec/uploaders/file_uploader_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'carrierwave/test/matchers'
+
+describe FileUploader do
+  include CarrierWave::Test::Matchers
+
+  let(:attachment) { create :attachment }
+  subject { FileUploader.new(attachment, :attachment) }
+
+  after { subject.remove! }
+
+  describe 'file path' do
+    it 'is separated for each environment' do
+      test_env = subject.store_dir
+
+      # stub Rails.env to return 'production'
+      allow(Rails).to receive(:env) { ActiveSupport::StringInquirer.new('production') }
+
+      prod_env = subject.store_dir
+
+      expect(test_env).to_not eq(prod_env)
+    end
+
+    it 'contains model name and id' do
+      expect(subject.store_dir).to include('attachment')
+      expect(subject.store_dir).to include(attachment.id.to_s)
+    end
+  end
+end
