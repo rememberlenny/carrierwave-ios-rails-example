324ea54b9a8c69dfcd8cc15e9a83934d5b235662
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-22 16:27:47 +0100

    handle RecordNotFound in API [#86686598]

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,15 @@
 class API::V1::ApiController < ActionController::Base
   respond_to :json
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  private
+
+  def record_not_found
+    render json: { errors: I18n.t('errors.record_not_found') }, status: :not_found
   end
 end
