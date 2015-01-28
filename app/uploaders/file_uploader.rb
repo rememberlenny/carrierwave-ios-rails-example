9f590dac6c032b16d1691940ea2c1eca6ce201bb
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-28 13:20:12 +0100

    make class method out of #supported_extensions in FileUploader [#87097702]

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -8,22 +8,24 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   def extension_white_list
-    supported_extensions
+    FileUploader.supported_extensions
   end
 
-  def supported_extensions
-    supported_video_extensions + supported_audio_extensions + supported_image_extensions
-  end
+  class << self
+    def supported_extensions
+      supported_video_extensions + supported_audio_extensions + supported_image_extensions
+    end
 
-  def supported_video_extensions
-    %w(asf avi dvr-ms m1v m4v mp2 mp2v mp4 mpe mpeg mpg mpv2 wm wmv)
-  end
+    def supported_video_extensions
+      %w(asf avi dvr-ms m1v m4v mp2 mp2v mp4 mpe mpeg mpg mpv2 wm wmv)
+    end
 
-  def supported_audio_extensions
-    %w(acc aif aifc aiff asf au flac m4v mp2 mp3 mpa snd wav wma)
-  end
+    def supported_audio_extensions
+      %w(acc aif aifc aiff asf au flac m4v mp2 mp3 mpa snd wav wma)
+    end
 
-  def supported_image_extensions
-    %w(bmp dib emf gif jfif jpe jpeg jpg png tif tiff wmf)
+    def supported_image_extensions
+      %w(bmp dib emf gif jfif jpe jpeg jpg png tif tiff wmf)
+    end
   end
 end
