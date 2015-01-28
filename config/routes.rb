fdda3423a1ea5e666f300822a52d86e11ba4ffa9
Bartosz Nowak <bartosz.nowak@netguru.pl>
2015-01-28 15:25:54 +0100

    add back the list of actions supported by api...

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :attachments do
+      resources :attachments, only: [:index, :show, :create, :destroy, :download, :supported_extensions] do
         get :download, on: :member
         get :supported_extensions, on: :collection
       end
