51cd3ce23ccd43364624cf894a881e15df6d95fb
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-03-06 14:23:47 +0100

    Use one before block [#89681746]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,14 +33,11 @@ RSpec.configure do |config|
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
   end
 
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
   end
 
   config.append_after(:each) do
