df899d495e90659e97613320f163d19dcf9bcd48
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-03-06 15:27:18 +0100

    Do not replicate code from gem [#89681676]

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,18 +1,5 @@
-class Attachment < ActiveRecord::Base
-  mount_uploader :file, FileUploader
-
+class Attachment < CarrierwaveIosRails::Attachment
   after_create :delayed_destroy
-  validates :file, presence: true
-
-  def filename
-    return unless file.present?
-    File.basename(file.path)
-  end
-
-  def file_data
-    return unless file.present?
-    file.read
-  end
 
   private
 
